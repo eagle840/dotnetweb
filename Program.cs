23a8e32eb6146f82db2608f54f5a30dd164dcafa
Nicholas Busby <eagle840@gmail.com>
2023-10-04 18:00:04 +0100

    add insights

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
